ef7bb2798e101643447cecb0cb1091169ecabed3
Alex B <aleksey-baryshnikov@users.noreply.github.com>
2019-04-16 17:44:12 +0600

    4-module 2-task merge conflict

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,15 +1,14 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
-const fs = require('fs');
-const LimitSizeStream = require('./LimitSizeStream');
-const LimitExceededError = require('./LimitExceededError');
 
 const receiveFile = require('./receiveFile');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
+
+  const pathname = url.parse(req.url).pathname.slice(1);
 
   if (pathname.includes('/') || pathname.includes('..')) {
     res.statusCode = 400;
@@ -32,7 +31,8 @@ server.on('request', (req, res) => {
       break;
 
     default:
-      end(501, 'Not implemented');
+      res.statusCode = 501;
+      res.end('Not implemented');
   }
 });
 
