3f5f2a418cec9771ad8ecb1b7081896a1b3a8c1e
Alex B <aleksey-baryshnikov@users.noreply.github.com>
2019-04-24 17:46:54 +0600

    6-module 1-task

--- 6-module/1-task/User.js
+++ 6-module/1-task/User.js
@@ -1,5 +1,33 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({});
+// https://mongoosejs.com/docs/guide.html
+// https://mongoosejs.com/docs/schematypes.html
+// https://mongoosejs.com/docs/validation.html
+
+const schema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        validate: [
+            {
+                validator(value) {
+                    return /^[-.\w]+@([\w-]+\.)+[\w-]{2,12}$/.test(value);
+                },
+                message: 'некорректный email'
+            }
+        ],
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    displayName: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, {
+        collection: 'users',
+        timestamps: true
+    });
 
 module.exports = mongoose.model('User', schema);
